61cc314f965327a36171bc8f87539d70c33fcb4a
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-09-26 03:43:58 -0400

    simplifying syntax

--- example.c
+++ example.c
@@ -4,15 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void gen_odd(blob data) {
-	int i;
-	gen_int(&i);
+int gen_odd() {
+	int i = gen_int();
 
 	if (i % 2 == 0) {
 		i++;
 	}
 
-	qc_return(int, i);
+	return i;
 }
 
 bool is_odd(blob data) {
@@ -29,24 +28,22 @@ bool both_less_than(blob data) {
 	return a < c && b < c;
 }
 
-void gen_digit_char(blob data) {
-	int i;
-	gen_int(&i);
+char gen_digit_char() {
+	int i = gen_int();
 
 	i %= 10;
 
 	char c = '0' + (char) i;
 
-	qc_return(char, c);
+	return c;
 }
 
-void gen_digit(blob data) {
-	int i;
-	gen_int(&i);
+int gen_digit() {
+	int i = gen_int();
 
 	i %= 10;
 
-	qc_return(int, i);
+	return i;
 }
 
 bool does_not_parse_to(blob data) {
@@ -65,32 +62,32 @@ bool does_not_have_an_h(blob data) {
 int main() {
 	qc_init();
 
-	gen gs[] = { gen_odd };
+	gen gs[] = { (gen) gen_odd };
 
 	print ps[] = { print_int };
 
 	// Are all odd numbers odd?
 	for_all(is_odd, 1, gs, ps, int);
 
-	gen gs2[] = { gen_int };
+	gen gs2[] = { (gen) gen_int };
 	print ps2[] = { print_int };
 
 	// Are all integers odd?
 	for_all(is_odd, 1, gs2, ps2, int);
 
-	gen gs3[] = { gen_int, gen_int, gen_int };
+	gen gs3[] = { (gen) gen_int, (gen) gen_int, (gen) gen_int };
 	print ps3[] = { print_int, print_int, print_int };
 
 	// Are any two integers less than a third integer?
 	for_all(both_less_than, 3, gs3, ps3, int);
 
-	gen gs4[] = { gen_digit_char, gen_digit };
+	gen gs4[] = { (gen) gen_digit_char, (gen) gen_digit };
 	print ps4[] = { print_char, print_int };
 
 	// Do any characters parse to a matching integer?
 	for_all(does_not_parse_to, 2, gs4, ps4, int);
 
-	gen gs5[] = { gen_string };
+	gen gs5[] = { (gen) gen_string };
 	print ps5[] = { print_string };
 
 	// Do any string pairs match?
--- qc.c
+++ qc.c
@@ -11,47 +11,45 @@ void qc_init() {
 	QC_INITIALIZED = true;
 }
 
-void gen_bool(blob data) {
+bool gen_bool() {
 	bool b = rand() % 2 == 0;
 
-	qc_return(bool, b);
+	return b;
 }
 
-void gen_int(blob data) {
+int gen_int() {
 	int i = rand();
 
-	qc_return(int, i);
+	return i;
 }
 
-void gen_char(blob data) {
+char gen_char() {
 	char c = (char) (rand() % 128);
 
-	qc_return(char, c);
+	return c;
 }
 
-void _gen_array(blob data, gen g, size_t size) {
+blob* _gen_array(gen g, size_t size) {
 	int len = rand() % 100;
 
-	blob arr = GC_MALLOC(len * size);
+	blob* arr = GC_MALLOC(len * size);
 
 	int i;
 	for (i = 0; i < len; i++) {
-		g(arr + i * size);
+		*(arr + i * size) = g();
 	}
 
-	qc_return(blob, arr);
+	return arr;
 }
 
-void gen_string(blob data) {
-	char* s;
+char* gen_string() {
+	char* s = (char*) gen_array(gen_char, char);
 
-	gen_array(&s, gen_char, char);
-
-	qc_return(char*, s);
+	return s;
 }
 
 void print_bool(blob data) {
-	bool b = (* (bool*) data);
+	bool b = qc_args(bool, 0, bool);
 
 	if (b) {
 		printf("true");
@@ -62,74 +60,22 @@ void print_bool(blob data) {
 }
 
 void print_int(blob data) {
-	int i = qc_args(int, 0, sizeof(int));
+	int i = qc_args(int, 0, int);
 
 	printf("%d", i);
 }
 
 void print_char(blob data) {
-	char c = qc_args(char, 0, sizeof(char));
+	char c = qc_args(char, 0, char);
 
 	printf("\'%c\'", c);
 }
 
 void print_string(blob data) {
-	char* s = qc_args(char*, 0, sizeof(char*));
+	char* s = qc_args(char*, 0, char*);
 
 	printf("%s", s);
 }
-
-// Syntax:
-//
-// For simplicity, _for_all is called using the for_all macro (see qc.h).
-//
-// In this example, for_all is testing whether all integers are odd (they are not).
-//
-// Every for_all call begins with a property function. For example, is_odd takes
-// an int and returns a bool.
-//
-// bool is_odd(void *data);
-//
-// In order to handle arbitrarily-typed property functions, qc uses a special
-// protocol, qc_args,  to pass test values to the property function.
-//
-// bool is_odd(void *data) {
-//    int n = qc_args(int, 0, sizeof(int));
-//
-//    return n % 2 == 1;
-// }
-//
-// qc_args' first argument is a type: int, char, int*, char*, int**, char**, ...
-// The second argument is the index (qc internally stores all test cases in a
-// single array).
-// The final argument is the maximum byte size of the property's input types. For
-// example, is_equal(short, int)'s maximum byte size would be sizeof(int).
-
-//
-// for_all's first argument is the name of such a property function. The next
-// argument is an array of generator functions. The is_odd property
-// function has a single argument, an integer. Thus each test case consists of a
-// random integer to be passed to the is_odd property function. More complicated
-// property functions (e.g. cmp(int, int)) may have multiple arguments, and
-// therefore require multiple generators.
-//
-// gen gs[] = { gen_int };
-//
-// When a test case fails, the values for which the property returns false will be
-// printed. For each generator, a corresponding printer function is necessary.
-// for_all is designed to test properties of arbitrary numbers of arbitrary types;
-// because C has no universal print(some_object) function, the framework user must
-// specify printer functions for each generator function. More complicated types,
-// such as trees, graphs, and linked lists require the framework user to write
-// custom printer functions, but the syntax remains the same.
-// 
-// print ps[] = { print_int };
-//
-// Finally, for_all requires the maximum byte size of the types to be passed to the
-// property function. This information helps for_all hold all test values in a single array,
-// which it passes to the test property.
-//
-// for_all(is_odd, 1, gs, ps, sizeof(int));
 
 void _for_all(prop property, int arglen, gen gs[], print ps[], size_t max_size) {
 	int i, j;
@@ -140,11 +86,11 @@ void _for_all(prop property, int arglen, gen gs[], print ps[], size_t max_size) 
 		return;
 	}
 
-	blob values = GC_MALLOC(arglen * max_size);
+	blob* values = GC_MALLOC(arglen * max_size);
 
 	for (i = 0; i < 100; i++) {
 		for (j = 0; j < arglen; j++) {
-			gs[j](values + j * max_size);
+			*(values + j * max_size) = gs[j]();
 		}
 
 		bool holds = property(values);
