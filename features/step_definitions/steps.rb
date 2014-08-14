ff582aee500fbbefa1700db7e258da7c511ea04c
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-14 14:20:44 -0500

    make

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,14 +7,16 @@ end
 Then(/^the output is correct for each test$/) do
   lines = @cucumber.split("\n")
 
-  lines.length.should >= 14
+  expect(lines.length).to be >= 15
 
-  lines[0 .. 1].join("\n").should == 'clang -O2 -Wall -Wextra -Wmost -Weverything -Wno-pointer-arith -o example example.c qc.c -lgc
-./example'
+  expect(lines[0 .. 2].join("\n")).to eq('mkdir -p bin/
+clang -O2 -Wall -Wextra -Wmost -Weverything -Wno-pointer-arith -o bin/example example.c qc.c -lgc
+bin/example')
 
-  lines[2].should == '+++ OK, passed 100 tests.'
-  lines[3 .. 4].join("\n").should =~ /^\*\*\* Failed!\n[0-9]+/m
-  lines[5 .. 8].join("\n").should =~ /^\*\*\* Failed!(\n[0-9]+){3}/m
-  lines[9 .. 11].join("\n").should =~ /^\*\*\* Failed!\n\'[0-9]\'\n[0-9]/m
-  lines[12 .. -1].join("\n").should =~ /^\*\*\* Failed!\n.*/m
+  expect(lines[3]).to eq('+++ OK, passed 100 tests.')
+  expect(lines[4]).to eq('+++ OK, passed 100 tests.')
+  expect(lines[5 .. 6].join("\n")).to match(/^\*\*\* Failed!\n[0-9]+/m)
+  expect(lines[7 .. 10].join("\n")).to match(/^\*\*\* Failed!(\n[0-9]+){3}/m)
+  expect(lines[11 .. 13].join("\n")).to match(/^\*\*\* Failed!\n\'[0-9]\'\n[0-9]/m)
+  expect(lines[14 .. -1].join("\n")).to match(/^\*\*\* Failed!\n.*/m)
 end
